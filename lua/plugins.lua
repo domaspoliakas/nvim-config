f736da7bf97291b2d0bcb5cd69d096fceca94b02
Domas Poliakas <domas@poliakas.dev>
2022-06-13 14:05:45 +0200

    Added some treesitter stuff

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -42,6 +42,7 @@ return require('packer').startup(function()
 
   use({ "nvim-treesitter/nvim-treesitter", run = ':TSUpdate' })
   use({ 'nvim-treesitter/nvim-treesitter-context'})
+  use({ 'nvim-treesitter/playground'})
 
   use({ 
     "tyru/open-browser-github.vim", 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,17 @@
 require'nvim-treesitter.configs'.setup {
   -- Modules and its options go here
   highlight = { enable = true },
-  folding = { enable = true }
+  folding = { enable = true },
+  playground = { enable = true }
   --[[ incremental_selection = { enable = true },
   textobjects = { enable = true }, ]]
 }
 
+
+require'treesitter-context'.setup {
+  patterns = {
+    json = {
+      'pair'
+    }
+  }
+}
