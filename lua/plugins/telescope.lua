bb55547d2db0e0e779b6899030aafa6941091f59
Domas Poliakas <domas@poliakas.dev>
2022-07-19 16:02:30 +0200

    Finally gone are the golden files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,13 @@
 require('telescope').setup {
+  defaults = {
+    file_ignore_patterns = { "golden" },
+    mappings = {
+      i = {
+        -- ["<C-t>"] = "layout.toggle_preview",
+        ["<C-h>"] = "which_key"
+      }
+    }
+  }
   --[[ defaults = {
     vimgrep_arguments = {
       "rg",
